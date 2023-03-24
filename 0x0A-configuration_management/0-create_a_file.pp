251dfbb5257141e411ce28cccf8a2b097df16e0f
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-24 19:58:38 +0100

    Change puppet to Puppets on t0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,6 +4,6 @@ file {'school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet',
+  content => 'I love Puppet',
   path    => '/tmp/school',
 }
