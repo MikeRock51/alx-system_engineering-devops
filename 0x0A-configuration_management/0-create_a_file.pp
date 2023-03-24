217ad7d0223af3f45b9883f906d199819d11c038
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-24 19:52:07 +0100

    Add task 1 and add path on 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Creates the file /tmp/school that contains 'I love Puppet' with 0744 permision
 
-file {'/tmp/school':
+file {'school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love puppet'
+  path    => '/tmp/school'
 }
