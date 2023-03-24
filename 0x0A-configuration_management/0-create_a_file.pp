a0651c4bf6922ccfa6407a3c41f29821f9abc8de
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-24 19:44:04 +0100

    Modify description comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,4 @@
-# Using Puppet, create a file in /tmp.
-  # Requirements:
-    # File path is /tmp/school
-    # File permission is 0744
-    # File owner is www-data
-    # File group is www-data
-    # File contains I love Puppet
-
+# Creates the file /tmp/school that contains 'I love Puppet' with 0744 permision
 
 file {'/tmp/school':
   mode    => '0744',
