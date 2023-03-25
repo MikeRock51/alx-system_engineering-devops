7f55da6ec7607124dabdb214cb9639976d3024b3
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-25 20:53:40 +0100

    Update task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,6 @@
 # Sets up an SSH configuration file
 
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-}
-
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
+exec {
+  "echo PasswordAuthentication no\n IdentityFile ~/.ssh/school >> /etc/ssh/ssh_config":
+  path    => '/bin/'
 }
