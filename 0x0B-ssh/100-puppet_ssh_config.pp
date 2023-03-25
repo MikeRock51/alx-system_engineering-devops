0736ca0629d888cd046ec52775e1e8bb63b7de1b
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-25 20:55:23 +0100

    Fix quotation issue on 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Sets up an SSH configuration file
 
 exec {
-  "echo PasswordAuthentication no\n IdentityFile ~/.ssh/school >> /etc/ssh/ssh_config":
+  'echo "PasswordAuthentication no\n IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
   path    => '/bin/'
 }
