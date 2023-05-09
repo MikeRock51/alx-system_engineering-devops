5d90bda38ddb65f305f340c4f7b738f8d1e7041f
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-09 19:30:24 +0100

    Add Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Makes a request to the reddit api"""
+
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    """
+        Prints the title of the top 10 hot posts
+        listed on a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(argv[1])
+    params = {"limit": 10}
+    headers = {"User-Agent": "Python3 3.10"}
+
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code != 200:
+        print(None)
+
+    datas = response.json().get('data').get('children')
+    for data in datas:
+        print(data.get('data').get('title'))
