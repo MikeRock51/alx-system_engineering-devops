97ce6acbe1e4ab5f9f4da206225e0ef6bfa066f3
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-09 18:33:15 +0100

    Afd task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Queries the reddit API"""
+
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(argv[1])
+
+    if not argv[1]:
+        return 0
+
+    headers = {
+            "User-Agent": "python3 3.10"
+            }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return 0
+
+    return response.json().get('data').get('subscribers')
