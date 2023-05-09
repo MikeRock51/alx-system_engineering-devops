984014bf0a5ea9f2ff848715812b635d0ee27c5a
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-09 20:57:25 +0100

    Add Task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Makes a recursive call to the Reddit API"""
+
+import requests
+
+
+def recurse(subreddit, params={}, hot_list=[]):
+    """
+        Returns a list of all titles of hot articles for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {"User-Agent": "Python3 3.10"}
+
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code != 200:
+        return None
+    datas = response.json().get('data').get('children')
+    for data in datas:
+        hot_list.append(data.get('data').get('title'))
+
+    nextPage = response.json().get('data').get('after')
+    if nextPage:
+        params["after"] = nextPage
+        recurse(subreddit, params, hot_list)
+
+    return hot_list
