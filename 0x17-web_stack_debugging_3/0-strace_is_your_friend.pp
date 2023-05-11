1650d754a69bbbbcea8740b30ecfa6d12cfb5a60
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-11 17:10:45 +0100

    Fix bug oj task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,5 +4,5 @@
 exec { 'replace-phpp-with-php':
   command     => '/bin/sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],
+  path        => ["/bin", "/usr/bin"],
 }
