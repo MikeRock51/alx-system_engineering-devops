ce9b9f10bcfef8350fdc009a33251e1f48f09b53
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-11 17:37:09 +0100

    Puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 
 
 exec { 'replace-phpp-with-php':
-  command     => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path	      => ['bin', 'usr/bin'],
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['bin', 'usr/bin'],
 }
