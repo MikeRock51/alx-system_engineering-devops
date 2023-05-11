8a2b6cb9facc40086f3598f091d3c314862fd977
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-11 17:33:34 +0100

    Bug fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,6 @@
 
 
 exec { 'replace-phpp-with-php':
-  command     => 'bin/sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  command     => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path	      => ['bin', 'usr/bin'],
 }
